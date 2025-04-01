387d87a72fb5316b08f5f48f5fa40cfd2363e16d
Zair Bulos <buloszair@gmail.com>
2025-04-01 16:40:45 -0300

    test: Extended FractionTest to cover isProper, isImproper, isEquivalent, multiply, and divide methods. #10

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -1,6 +1,8 @@
 package es.upm.miw.iwvg_devops.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,5 +41,44 @@ class FractionTest {
     @Test
     void testDecimal() {
         assertEquals(2.0, fraction.decimal());
+    }
+
+    @Test
+    void testIsProper() {
+        assertFalse(fraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertTrue(fraction.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        Fraction fraction1 = new Fraction(8, 4);
+        Fraction fraction2 = new Fraction(3, 2);
+
+        assertTrue(fraction.isEquivalent(fraction1));
+        assertFalse(fraction.isEquivalent(fraction2));
+    }
+
+    @Test
+    void testMultiply() {
+        Fraction fraction1 = new Fraction(3, 5);
+
+        Fraction result = fraction.multiply(fraction1);
+
+        assertEquals(12, result.getNumerator());
+        assertEquals(10, result.getDenominator());
+    }
+
+    @Test
+    void testDivide() {
+        Fraction fraction1 = new Fraction(3, 5);
+
+        Fraction result = fraction.divide(fraction1);
+
+        assertEquals(20, result.getNumerator());
+        assertEquals(6, result.getDenominator());
     }
 }
